0514209d727cf9e712a34d12b03dd0c9415fe145
Nils <nilsvandepitte9@gmail.com>
2025-06-09 15:27:57 +0200

    Integrate database sync functionality in application initialization.

--- code/app.js
+++ code/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sync = require('./infrastructure/database/config/sync');
 
 const app = express();
 
@@ -7,5 +8,10 @@ require('./config/middleware')(app, express);
 
 // Routes
 // require('./config/routes')(app);
+
+// Models
+sync()
+  .then(() => console.log('Database synced'))
+  .catch(err => console.log(err));
 
 module.exports = app;
