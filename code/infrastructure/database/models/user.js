8e4636e2559dab59844b325113d616bdd9e99da6
Nils <nilsvandepitte9@gmail.com>
2025-06-09 13:52:47 +0200

    Add Sequelize user model with schema definition

---
+++ code/infrastructure/database/models/user.js
@@ -0,0 +1,31 @@
+﻿const config = require('../config/connection');
+const {DataTypes} = require("sequelize");
+
+const User = config.define('user', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: crypto.randomUUID(),
+    primaryKey: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'user'
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true
+  }
+});
+
+module.exports = User;
