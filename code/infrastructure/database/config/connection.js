64b4b2a40c91472fb3e0ce892dba1a350e4a73b1
Nils <nilsvandepitte9@gmail.com>
2025-06-09 13:52:42 +0200

    Add Sequelize database connection configuration

---
+++ code/infrastructure/database/config/connection.js
@@ -0,0 +1,30 @@
+﻿const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize({
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+      },
+    }
+  });
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+_ = testConnection();
+
+
+module.exports = sequelize;
